77588fa082fa40981ba39537bb30603881a5ce5b
ErenSanjay <itssanjaykj@gmail.com>
2026-09-02 22:00:53 +0530

    corrected the dates

--- src/data/letters.ts
+++ src/data/letters.ts
@@ -2,21 +2,21 @@ export const letters = [
   {
     id: 1,
     title: "My First PDF Letter",
-    date: "September 1, 2026",
+    date: "August 15, 2026",
     file: "/letters/pdf.pdf",
     type: "pdf" as const
   },
   {
     id: 2,
     title: "Another Letter For You ❤️",
-    date: "September 5, 2026",
+    date: "August 24, 2026",
     file: "/letters/pdf2.pdf",
     type: "pdf" as const
   },
   {
     id: 3,
     title: "The Third Letter 💌",
-    date: "September 6, 2026",
+    date: "September 2, 2026",
     file: "/letters/pdf3.pdf",
     type: "pdf" as const
   }
